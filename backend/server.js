672f12930949c6df3f5f6c1636c2d60804531a39
Bhagya Wijenayaka <bhagyasubhashini139@gmail.com>
2026-01-31 01:58:09 +0530

    Routes Setup

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import morgan from 'morgan';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import productRoutes from './routes/productRoutes.js';
+
 dotenv.config();
 
 
@@ -15,9 +17,7 @@ app.use(cors()); //middleware to enable CORS
 app.use(helmet()); //helmet is a security middleware that helps to secure Express apps by setting various HTTP headers.
 app.use(morgan('dev')); //logs the requests made to the server
 
-app.get('/test', (req, res) => {
-  res.send('Hello from the test route!');
-});
+app.use('/api/products', productRoutes);
 
 app.listen(PORT, () => {
   console.log('Server is running on port ' + PORT);
